f3e15c6dedec0717ef95a837a3809128a04251be
LalaIAm <lthorud12@gmail.com>
2020-01-28 00:32:29 -0600

    Updates

---
+++ precache-manifest.751e7b186f58831303b9bc894299cc65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bb82469dbb9a250968440388696b05",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "21663841603c124d9515",
+    "url": "/quote-machine/static/css/main.d03dfef3.chunk.css"
+  },
+  {
+    "revision": "58fd05a64a05fe215470",
+    "url": "/quote-machine/static/js/2.4e07526f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quote-machine/static/js/2.4e07526f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21663841603c124d9515",
+    "url": "/quote-machine/static/js/main.4c18b67b.chunk.js"
+  },
+  {
+    "revision": "88851f92b500f76fa476",
+    "url": "/quote-machine/static/js/runtime-main.d2b50f78.js"
+  }
+]);
